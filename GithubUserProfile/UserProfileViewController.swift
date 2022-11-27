3dbe7019abb8abdcf90c0e35e9795779b0a86956
aaronsleepy <aaron@kmong.com>
2022-11-27 20:15:15 +0900

    feat: View Object와 Controller 연결

--- GithubUserProfile/UserProfileViewController.swift
+++ GithubUserProfile/UserProfileViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
