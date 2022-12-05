77dac205c822e19cf2de64602db62a387c8572f4
aaronsleepy <aaron@kmong.com>
2022-12-05 18:50:43 +0900

    refactor: Group 추가 및 UserProfileViewModel 추가

---
+++ GithubUserProfile/UserProfileViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserProfileViewModel.swift
+//  GithubUserProfile
+//
+//  Created by Aaron on 2022/12/05.
+//
+
+import Foundation
+
+final class UserProfileViewModel {
+    // Data => Output
+    
+    // User Action => Input
+}
